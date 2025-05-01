fce7f2d8673d05885a653dbe2e54261b6ca0fc7a
AlexTe32 <trajcevskialeksandar861@gmail.com>
2025-05-01 21:21:18 +0200

    1.5.2025
    Cat done, Mouse done, Pathfinding done
    
    Simluation as a Function Done
    
    Writing in exel DONE
    
    just need to run it a 10.000 tiems
    (╥﹏╥)

--- Mouse.py
+++ Mouse.py
@@ -4,7 +4,7 @@ class Mouse:
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.assigned = False  # This initializes the 'assigned' attribute
+        self.assigned = False
 
     def get_position(self):
         return self.row, self.col
@@ -13,16 +13,15 @@ class Mouse:
         return self.assigned
 
     def assign_to_cat(self, cat):
-        self.assigned = True  # Mark mouse as assigned to a cat
+        self.assigned = True
 
     def unassign(self):
-        self.assigned = False  # Mark mouse as unassigned (for when it is caught)
+        self.assigned = False
 
-    # Function to calculate Manhattan distance
     def manhattan(self, pos1, pos2):
         return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
 
-    def move_random(self, max_rows, max_cols, obstacles=set()):
+    def move_random(self, max_rows, max_cols, obstacles=set(), occupied=set()):
         directions = ['UP', 'DOWN', 'LEFT', 'RIGHT']
         random.shuffle(directions)
         for direction in directions:
@@ -36,21 +35,18 @@ class Mouse:
                 new_c -= 1
             elif direction == 'RIGHT' and c < max_cols - 1:
                 new_c += 1
-            if (new_r, new_c) not in obstacles:
+            if (new_r, new_c) not in obstacles and (new_r, new_c) not in occupied:
                 self.row, self.col = new_r, new_c
                 break
 
-    def move_away_from_cats(self, cats, grid_rows, grid_cols, obstacles=set()):
+    def move_away_from_cats(self, cats, grid_rows, grid_cols, obstacles=set(), occupied=set()):
         if not cats:
             return
 
         my_pos = self.get_position()
-
-        # Find nearest cat
         nearest_cat = min(cats, key=lambda c: self.manhattan(my_pos, c.get_position()))
         danger_pos = nearest_cat.get_position()
 
-        # Try all 4 directions and pick the one that increases distance from cat
         best_move = my_pos
         best_dist = self.manhattan(my_pos, danger_pos)
 
@@ -58,7 +54,12 @@ class Mouse:
             new_r = self.row + dr
             new_c = self.col + dc
             new_pos = (new_r, new_c)
-            if 0 <= new_r < grid_rows and 0 <= new_c < grid_cols and new_pos not in obstacles:
+            if (
+                0 <= new_r < grid_rows and
+                0 <= new_c < grid_cols and
+                new_pos not in obstacles and
+                new_pos not in occupied
+            ):
                 dist = self.manhattan(new_pos, danger_pos)
                 if dist > best_dist:
                     best_dist = dist
